97d7938105b6e65b12d933a0c83129419a78cb68
kamarlaylatt <kamarehtha96@gmail.com>
2026-02-01 00:25:55 +0700

    add title hymn detail

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,21 +1,24 @@
 import { Stack } from "expo-router";
-import { useTheme } from 'react-native-paper';
+import { useTheme } from "react-native-paper";
 
 export default function RootLayout() {
   const theme = useTheme();
 
-  return <Stack
-    screenOptions={{
-      headerShown: true,
-      headerTitleStyle: { color: theme.colors.onSurface },
-      headerTintColor: theme.colors.onSurface,
-      headerStyle: { backgroundColor: theme.colors.surface },
-      contentStyle: { backgroundColor: theme.colors.background }
-    }}
-  >
-    <Stack.Screen redirect={true} name="index" />
-    <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-    <Stack.Screen name="song/[slug]" options={{ title: "Song Detail" }} />
-    <Stack.Screen name="suggest-song" options={{ title: "Suggest a Song" }} />
-  </Stack>;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitleStyle: { color: theme.colors.onSurface },
+        headerTintColor: theme.colors.onSurface,
+        headerStyle: { backgroundColor: theme.colors.surface },
+        contentStyle: { backgroundColor: theme.colors.background },
+      }}
+    >
+      <Stack.Screen redirect={true} name="index" />
+      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+      <Stack.Screen name="song/[slug]" options={{ title: "Song Detail" }} />
+      <Stack.Screen name="hymn/[number]" options={{ title: "Hymn Detail" }} />
+      <Stack.Screen name="suggest-song" options={{ title: "Suggest a Song" }} />
+    </Stack>
+  );
 }
